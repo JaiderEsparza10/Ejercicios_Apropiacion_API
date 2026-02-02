f4fead72602e27ee9f2b91835bfa4d093004ce34
Jaider Esparza <xenno2003@gmail.com>
2026-02-02 17:45:44 -0500

    Se creó la función para el ejercicio 10 y seguido se dió respuesta a ese punto

--- peticiones/solucionAnalizar-Verificar.js
+++ peticiones/solucionAnalizar-Verificar.js
@@ -1,5 +1,6 @@
 // Función para realizar una petición GET a un recurso específico
 const peticion10Get = async () => {
+    console.log(`----- SOLUCIÓN 9 -----`);
     // Realizamos la petición GET para obtener el post con ID 1
     const recursoEliminado = await fetch(`http://localhost:3000/posts/1`);
     // Convertimos la respuesta a formato JSON
@@ -9,3 +10,4 @@ const peticion10Get = async () => {
 }
 // Llamamos a la función para ejecutar la petición GET
 peticion10Get();
+
